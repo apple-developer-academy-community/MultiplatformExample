79c3adcc2143bf6f71c5ae00da6806322bfa07a6
jsh9611 <jangsh9611@naver.com>
2022-11-23 21:47:41 +0900

    [FEAT]  start project

---
+++ MultiplatformExample/MultiplatformExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformExampleApp.swift
+//  MultiplatformExample
+//
+//  Created by SeongHoon Jang on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
